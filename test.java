de22732b94c78660007318851c06a8d02947909a
Lokesh Lulla <Lokesh_Lulla@epam.com>
2017-08-04 16:22:14 +0530

    Adding test class

---
+++ test.java
@@ -0,0 +1,18 @@
+
+public class test {
+
+	public static void main(String[] args) {
+	
+		int a[]={5,9,2,8,10,1};
+		int l=a.length;
+		for(int i=0;i<l;i++)
+		{
+			int temp;
+			temp=a[i];
+			//a[i]=
+			
+		}
+
+	}
+
+}
